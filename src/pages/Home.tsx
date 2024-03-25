bc33fc01ed89f50bb9a068b7d66c0351e3a34656
Kehinde-Agboola <kehindeagboola52@gmail.com>
2024-03-25 17:57:17 +0100

    error-debugged

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,6 +8,15 @@ export const Home = () => {
   return (
     <>
       <Hero />
+
+      <StyledApp>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non rerum
+          praesentium sint voluptatum cum laboriosam, vel nemo ullam fugiat
+          soluta debitis atque consequatur nam dolores alias reiciendis deserunt
+          mollitia porro.
+        </p>
+      </StyledApp>
     </>
   );
 };
