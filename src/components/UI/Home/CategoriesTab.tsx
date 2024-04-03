65d044fd5a91021c7c228856215b4951893c833c
Kehinde-Agboola <kehindeagboola52@gmail.com>
2024-04-03 17:35:56 +0100

    categories-tap

--- src/components/UI/Home/CategoriesTab.tsx
+++ src/components/UI/Home/CategoriesTab.tsx
@@ -1,5 +1,7 @@
 import Tabs from "../../Tabs/Tabs";
 import Tab from "../../Tabs/Tab";
+import { Gallary } from "./Gallary";
+// import Swiper from "swiper";
 // import styled from "styled-components";
 // import tw from "twin.macro";
 
@@ -7,7 +9,9 @@ export const CategoriesTab = () => {
   return (
     <>
       <Tabs>
-        <Tab title="Indoor-Plants">Indoor plants works</Tab>
+        <Tab title="Indoor-Plants">
+          <Gallary />
+        </Tab>
         <Tab title="Outdoor-Plants"> Outdoor plants works</Tab>
         <Tab title="Small-plants">Small plants works</Tab>
         <Tab title="Office-Plants">Office-plants-works</Tab>
