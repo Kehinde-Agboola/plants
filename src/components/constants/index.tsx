b0f7f64d024e707eef798e30756d9da49ada67cb
Kehinde-Agboola <kehindeagboola52@gmail.com>
2024-04-03 17:35:24 +0100

    pictures

--- src/components/constants/index.tsx
+++ src/components/constants/index.tsx
@@ -10,3 +10,35 @@ export const navigationLinks: Array<NavigationInterface> = [
   { title: "About", pathname: "/about", scrollOffset: -70 },
   { title: "Contact", pathname: "/contact", scrollOffset: -100 },
 ];
+
+export const slideImg = [
+  "./src/assets/plant1.png",
+  "./src/assets/plant2.png",
+  "./src/assets/plant3.png",
+  "./src/assets/plant4.png",
+  "./src/assets/plants5.png",
+  "./src/assets/plant6.png",
+  "./src/assets/plant7.png",
+  "./src/assets/plant8.png",
+  "./src/assets/plant9.png",
+  "./src/assets/plant10.png",
+  "./src/assets/plant11.png",
+  "./src/assets/plant12.png",
+];
+export const slide = [
+  "./src/assets/plants/p1.svg",
+  "./src/assets/plants/p2.svg",
+  "./src/assets/plants/p3.svg",
+  "./src/assets/plants/p4.svg",
+  "./src/assets/plants/p2.svg",
+  "./src/assets/plants/p6.svg",
+  "./src/assets/plants/p7.svg",
+  "./src/assets/plants/p2.svg",
+  "./src/assets/plants/p1.svg",
+  "./src/assets/plants/p6.svg",
+  "./src/assets/plants/p7.svg",
+];
+
+<div>
+  <img src="" alt="" />
+</div>;
