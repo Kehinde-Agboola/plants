57a5d91a1c30fb8481e84c9040f88d4e0b037e8c
Kehinde-Agboola <kehindeagboola52@gmail.com>
2024-04-08 11:02:31 +0100

    added chakra provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App.tsx";
 import { ClickContextProvider } from "./context/click-context.tsx";
 import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ClickContextProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/*" element={<App />} />
-        </Routes>
-      </BrowserRouter>
-    </ClickContextProvider>
+    <ChakraProvider>
+      <ClickContextProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/*" element={<App />} />
+          </Routes>
+        </BrowserRouter>
+      </ClickContextProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
