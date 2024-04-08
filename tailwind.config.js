471f7da61709c64a2c9aac0ff3de9f7f4dbb1c00
Kehinde-Agboola <kehindeagboola52@gmail.com>
2024-04-08 11:21:34 +0100

    edited taiwindconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,13 +10,6 @@ export default {
       },
       colors: {
         body: "#FCF9F3",
-        primary: "#f42c37",
-        brandYellow: "#fdc62e",
-        brandGreen: "#2dcc6f",
-        brandBlue: "#1376f4",
-        brandWhite: "#eeeeee",
-        brand: "#f8f1e3",
-        darkPink: "#C8B8A5",
       },
       backgroundImage: {
         hero: "linear-gradient(rgba(0,0,0,.3), rgba(0,0,0,.3)), url('/src/assets/plant11.png')",
