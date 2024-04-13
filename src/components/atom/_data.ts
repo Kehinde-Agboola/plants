d0f7fa07ccc5c946d8785122c62fecf45cd6e5cf
Kehinde-Agboola <kehindeagboola52@gmail.com>
2024-04-13 16:37:14 +0100

    added plants

--- src/components/atom/_data.ts
+++ src/components/atom/_data.ts
@@ -38,8 +38,7 @@ export const products = [
     currency: "USD",
     price: 199,
     flag: "new",
-    imageUrl:
-      "https://images.unsplash.com/photo-1602024242516-fbc9d4fda4b6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80",
+    imageUrl: "./src/assets/plants/jj.jpg",
     rating: 4,
     ratingCount: 1,
     description:
@@ -53,8 +52,7 @@ export const products = [
     price: 199,
     salePrice: 179.99,
     flag: "on-sale",
-    imageUrl:
-      "https://images.unsplash.com/photo-1509941943102-10c232535736?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80",
+    imageUrl: "./src/assets/plants/jj.jpg",
     rating: 4,
     ratingCount: 12,
     description:
@@ -66,8 +64,7 @@ export const products = [
     name: "Marble Leather",
     currency: "USD",
     price: 199,
-    imageUrl:
-      "https://images.unsplash.com/photo-1564594985645-4427056e22e2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80",
+    imageUrl: "./src/assets/plants/jj.jpg",
     rating: 4,
     ratingCount: 12,
     description:
@@ -79,8 +76,7 @@ export const products = [
     name: "Silve wolf",
     currency: "GBP",
     price: 199,
-    imageUrl:
-      "https://images.unsplash.com/photo-1522312346375-d1a52e2b99b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=680&q=80",
+    imageUrl: "./src/assets/plants/jj.jpg",
     rating: 5,
     ratingCount: 1,
     description:
